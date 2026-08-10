2c97fbb9d7107607e62c79ffc31b033a3487bb43
ravoeee-droid <ravoeee@gmail.com>
2026-08-10 08:22:37 +0200

    Track microsite intent safely

---
+++ app/a/[id]/TrackView.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { useEffect } from "react";
+export default function TrackView({leadId}:{leadId:string}){
+ useEffect(()=>{void fetch("/api/track",{method:"POST",headers:{"content-type":"application/json"},body:JSON.stringify({leadId,type:"microsite_view"}),keepalive:true}).catch(()=>undefined)},[leadId]);
+ return null;
+}
