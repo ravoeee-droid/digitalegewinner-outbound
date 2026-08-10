005c241af06107c1820648cc23f3e80b7df273b4
ravoeee-droid <ravoeee@gmail.com>
2026-08-10 08:29:08 +0200

    Expose Lead Finder and encrypted API vault

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import EnergyRadarLive from "./ui/EnergyRadarLive";
+import IntegrationVaultWidget from "./ui/IntegrationVaultWidget";
+import LeadFinderWidget from "./ui/LeadFinderWidget";
 
 export default function Page() {
-  return <EnergyRadarLive />;
+  return (
+    <>
+      <EnergyRadarLive />
+      <LeadFinderWidget />
+      <IntegrationVaultWidget />
+    </>
+  );
 }
