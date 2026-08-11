2b6b4fcefd134babcedbb58d8887c4f885b31635
ravoeee-droid <ravoeee@gmail.com>
2026-08-11 16:01:22 +0200

    Switch app to Digitale Gewinner Outbound OS

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import EnergyRadarLive from "./ui/EnergyRadarLive";
+import DigitaleGewinnerOutboundOS from "./ui/DigitaleGewinnerOutboundOS";
 import InboxWidget from "./ui/InboxWidget";
 import IntegrationVaultWidget from "./ui/IntegrationVaultWidget";
 import LeadFinderWidget from "./ui/LeadFinderWidget";
@@ -9,7 +9,7 @@ import NoShowRescueWidget from "./ui/NoShowRescueWidget";
 export default function Page() {
   return (
     <>
-      <EnergyRadarLive />
+      <DigitaleGewinnerOutboundOS />
       <InboxWidget />
       <LeadFinderWidget />
       <CampaignLabWidget />
