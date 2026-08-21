4c263fca22b2a59cc46677a7f5fd87e20336fa86
ravoeee-droid <ravoeee@gmail.com>
2026-08-21 11:06:47 +0200

    feat: integrate Studio V3 navigation and WCAG skip target

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import PflegeLaunchOS from "./ui/PflegeLaunchOS";
 import CloudTalkPhone from "./ui/CloudTalkPhone";
+import StudioNavPortal from "./ui/StudioNavPortal";
 
 export default function Page() {
   return (
     <>
-      <PflegeLaunchOS />
+      <a className="wcag-skip-link" href="#outbound-workspace">Zum Hauptinhalt springen</a>
+      <div id="outbound-workspace" tabIndex={-1}>
+        <PflegeLaunchOS />
+      </div>
+      <StudioNavPortal />
       <CloudTalkPhone />
     </>
   );
