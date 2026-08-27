bf4a1cf51e23a6a4ffd9727890749e8bc7b89869
ravoeee-droid <ravoeee@gmail.com>
2026-08-27 09:41:13 +0200

    feat: make closing cockpit the default Pflege workspace

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import PflegeProRuntime from "./ui/PflegeProRuntime";
+import PflegeCloserRuntime from "./ui/PflegeCloserRuntime";
 import CloudTalkPhone from "./ui/CloudTalkPhone";
 import DomainMailCenter from "./ui/DomainMailCenter";
 import DomainMailBridge from "./ui/DomainMailBridge";
@@ -6,8 +6,10 @@ import DomainMailBridge from "./ui/DomainMailBridge";
 export default function Page() {
   return (
     <>
-      <a className="wcag-skip-link" href="#main-sales-os">Zum Hauptinhalt springen</a>
-      <PflegeProRuntime />
+      <a className="wcag-skip-link" href="#pflege-closing-os">Zum Hauptinhalt springen</a>
+      <div id="pflege-closing-os">
+        <PflegeCloserRuntime />
+      </div>
       <CloudTalkPhone />
       <DomainMailCenter />
       <DomainMailBridge />
