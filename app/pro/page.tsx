bb56ace5559522da36a391f49b86dd7323950e81
ravoeee-droid <ravoeee@gmail.com>
2026-08-27 09:41:00 +0200

    feat: preserve advanced Pflege Pro workspace

---
+++ app/pro/page.tsx
@@ -0,0 +1,15 @@
+import PflegeProRuntime from "../ui/PflegeProRuntime";
+import CloudTalkPhone from "../ui/CloudTalkPhone";
+import DomainMailCenter from "../ui/DomainMailCenter";
+import DomainMailBridge from "../ui/DomainMailBridge";
+
+export default function ProPage() {
+  return (
+    <>
+      <PflegeProRuntime />
+      <CloudTalkPhone />
+      <DomainMailCenter />
+      <DomainMailBridge />
+    </>
+  );
+}
