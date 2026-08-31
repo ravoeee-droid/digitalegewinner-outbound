1728846b2342201ca742f10bc7a8af13c54022ad
ravoeee-droid <ravoeee@gmail.com>
2026-08-31 14:10:15 +0200

    feat: surface OpenOutreach sync in outbound center

--- app/outbound/page.tsx
+++ app/outbound/page.tsx
@@ -1,7 +1,13 @@
+import OpenOutreachSyncButton from "@/app/ui/OpenOutreachSyncButton";
 import OutboundEngineRuntime from "@/app/ui/OutboundEngineRuntime";
 
 export const dynamic = "force-dynamic";
 
 export default function OutboundPage() {
-  return <OutboundEngineRuntime />;
+  return (
+    <>
+      <OpenOutreachSyncButton />
+      <OutboundEngineRuntime />
+    </>
+  );
 }
