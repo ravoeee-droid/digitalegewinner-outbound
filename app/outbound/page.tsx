5f03ab3ce9fafea1ca022efbd5c5a58847cb85a4
ravoeee-droid <ravoeee@gmail.com>
2026-08-31 14:08:32 +0200

    feat: add outbound command center page

---
+++ app/outbound/page.tsx
@@ -0,0 +1,7 @@
+import OutboundEngineRuntime from "@/app/ui/OutboundEngineRuntime";
+
+export const dynamic = "force-dynamic";
+
+export default function OutboundPage() {
+  return <OutboundEngineRuntime />;
+}
