23e5eb3269d690e67c3089c4c45993cffee807bc
ravoeee-droid <ravoeee@gmail.com>
2026-09-08 10:26:19 +0200

    Apply cleaner visual hierarchy to outbound OS

--- app/outbound/page.tsx
+++ app/outbound/page.tsx
@@ -3,17 +3,18 @@ import WebsitePipelineShortcuts from "@/app/ui/WebsitePipelineShortcuts";
 import CloudTalkPhone from "@/app/ui/CloudTalkPhone";
 import DomainMailCenter from "@/app/ui/DomainMailCenter";
 import DomainMailBridge from "@/app/ui/DomainMailBridge";
+import clean from "@/app/ui/revenue-outbound-clean.module.css";
 
 export const dynamic = "force-dynamic";
 
 export default function OutboundPage() {
   return (
-    <>
+    <div className={clean.root}>
       <RevenueOutboundOS />
       <WebsitePipelineShortcuts />
       <CloudTalkPhone />
       <DomainMailCenter />
       <DomainMailBridge />
-    </>
+    </div>
   );
 }
