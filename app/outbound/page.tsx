49d3a0601dec9a936f00cf98dd37f534f74b6ec7
ravoeee-droid <ravoeee@gmail.com>
2026-09-14 14:51:01 +0700

    feat(mail): surface email launcher in command center

--- app/outbound/page.tsx
+++ app/outbound/page.tsx
@@ -1,5 +1,6 @@
 import OutboundCommandCenter from "@/app/ui/OutboundCommandCenter";
 import IntegrationVaultWidget from "@/app/ui/IntegrationVaultWidget";
+import MailLauncher from "@/app/ui/MailLauncher";
 import "./crm-buildstream-theme.css";
 
 export const dynamic = "force-dynamic";
@@ -7,6 +8,7 @@ export const dynamic = "force-dynamic";
 export default function OutboundPage() {
   return <>
     <OutboundCommandCenter />
+    <MailLauncher />
     <IntegrationVaultWidget />
   </>;
 }
